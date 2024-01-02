a6acb186cddfee6715f0898203e922bba356d68a
shinohara320 <daudmanuwu@gmail.com>
2024-01-02 16:25:36 +0900

    sticky navbar & fix z-index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,44 @@
-import React, { useState } from 'react';
+  import React, { useState } from 'react';
 
-const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleMenuClick = () => {
-    setIsMenuOpen(!isMenuOpen);
+    const handleMenuClick = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+      <div className='sticky top-0 z-50'>
+        <nav className="bg-white shadow dark:bg-gray-800">
+          <div className="hidden md:flex container items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+            <a href="#" className="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Campus Project</a>
+            <a href="#" className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">RSVP</a>
+            <a href="#" className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Venue</a>
+            <a href="#" className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contact Us</a>
+            <a href="#" className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Our Team</a>
+          </div>
+          {/* MOBILE */}
+          <div className="md:hidden p-2 flex justify-end">
+            <button onClick={handleMenuClick}>
+              <img
+                src="https://img.icons8.com/ios-filled/50/FFFFFF/menu--v6.png"
+                className='w-10 h-10'
+                alt="menu-squared-2"
+              />
+            </button>
+          </div>
+          {isMenuOpen && (
+            <div className='md:hidden flex flex-col items-center text-center'>
+              <a href="#" className="md:p-4 py-2 block hover:bg-blue-500">Campus Project</a>
+              <a href="#" className="md:p-4 py-2 block hover:bg-blue-500">RSVP</a>
+              <a href="#" className="md:p-4 py-2 block hover:bg-blue-500">Venue</a>
+              <a href="#" className="md:p-4 py-2 block hover:bg-blue-500">Contact Us</a>
+              <a href="#" className="md:p-4 py-2 block hover:bg-blue-500">Our Team</a>
+            </div>
+          )}
+        </nav>
+      </div>
+    );
   };
 
-  return (
-    <div>
-      <nav className="bg-white shadow dark:bg-gray-800">
-        <div className="hidden md:flex container items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
-          <a href="#" className="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Campus Project</a>
-          <a href="#" className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">RSVP</a>
-          <a href="#" className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Venue</a>
-          <a href="#" className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contact Us</a>
-          <a href="#" className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Our Team</a>
-        </div>
-        {/* MOBILE */}
-        <div className="md:hidden p-2 flex justify-end">
-          <button onClick={handleMenuClick}>
-            <img
-              src="https://img.icons8.com/ios-filled/50/FFFFFF/menu--v6.png"
-              className='w-10 h-10'
-              alt="menu-squared-2"
-            />
-          </button>
-        </div>
-        {isMenuOpen && (
-          <div className='md:hidden flex flex-col items-center text-center'>
-            <a href="#" className="md:p-4 py-2 block hover:bg-blue-500">Campus Project</a>
-            <a href="#" className="md:p-4 py-2 block hover:bg-blue-500">RSVP</a>
-            <a href="#" className="md:p-4 py-2 block hover:bg-blue-500">Venue</a>
-            <a href="#" className="md:p-4 py-2 block hover:bg-blue-500">Contact Us</a>
-            <a href="#" className="md:p-4 py-2 block hover:bg-blue-500">Our Team</a>
-          </div>
-        )}
-      </nav>
-    </div>
-  );
-};
-
-export default Navbar;
+  export default Navbar;
